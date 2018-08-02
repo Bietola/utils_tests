839e48f24e2ae5e2d3da57996065336860261317
Stefano Montesi <dincio.montesi@gmail.com>
2018-08-02 12:54:55 +0200

    Worked up working example.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
+#include <memory>
 
-#include "utils.h"
+#include "utils/prototype.h"
+
+class Greeter :
+    public utl::clonable_interface<Greeter> {
+
+    protected:
+        std::string mAdjective = "wonderful";
+
+    public:
+        void setAdjective(const std::string& adj) { mAdjective = adj; }
+
+        virtual ~Greeter() { std::cout << "me dead now..." << std::endl; }
+
+        virtual void greet() const = 0;
+};
+
+class WorldGreeter : 
+    public utl::clonable<Greeter, WorldGreeter> {
+
+    private:
+        std::string mAdditionalAdjective = "wonderful";
+
+    public:
+        WorldGreeter(const std::string& adj): mAdditionalAdjective(adj) {}
+
+        void greet() const override {
+            std::cout << "hello " + mAdjective + ", " + mAdditionalAdjective + " world!" << std::endl;
+        }
+};
 
 int main() {
-    std::cout << greet("world") << std::endl;
+    std::unique_ptr<Greeter> greeter1 = std::make_unique<WorldGreeter>("peaceful");
+    std::unique_ptr<Greeter> greeter2 = greeter1->clone();
+    greeter2->setAdjective("cruel");
+
+    greeter1->greet();
+    greeter2->greet();
 
     return 0;
 }
--- src/utils.h
+++
@@ -1,7 +0,0 @@
-#pragma once 
-
-#include <string>
-
-auto greet(const std::string& toGreet) {
-    return "Hello " + toGreet + "!!!";
-}
